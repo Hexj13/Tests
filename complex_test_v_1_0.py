91c843d66c1569a421984d3103320745fc605b76
Vladimir Lapchinsky <vladimir.lapchinsky@gmail.com>
2017-02-03 16:38:00 +0300

    Доб. комменты
    Закончил обработчик для комплексного тестирования внутри Flexbby.

--- complex_test_v_1_0.py
+++ complex_test_v_1_0.py
@@ -88,6 +88,7 @@ search_element = wait.until(
 # Закрываем договор
 search_element = wait.until(
 	EC.element_to_be_clickable((By.ID, ok_button_id))).click()
+time.sleep(5)
 # Открываем подменю "Штрих-кода"
 search_element = wait.until(
 	EC.element_to_be_clickable((By.XPATH, barcode_xpath))).click()
@@ -141,26 +142,41 @@ search_element = wait.until(
 # Выбераем сотрудника
 search_element = wait.until(
 	EC.element_to_be_clickable((By.XPATH, delegation_member_xpath))).click()
-action.double_click().perform()  # Двойной клик
+time.sleep(1)
+action.send_keys(Keys.RETURN).perform()  # Enter
+time.sleep(1)
 # Закрываем окно с выбором сотрудников
 search_element = wait.until(
 	EC.element_to_be_clickable((By.XPATH, close_button_xpath))).click()
+# Спим
+time.sleep(10)
 # Нажимаем кнопку добавления контрагента
 search_element = wait.until(
 	EC.element_to_be_clickable((By.XPATH, linkage_button_xpath))).click()
-#
+# Нажимаем Заказчик
 search_element = wait.until(
 	EC.element_to_be_clickable((By.XPATH, employee_contragent_add_xpath))).click()
-#
+# Нажимаем Юр. Лицо
 search_element = wait.until(
 	EC.element_to_be_clickable((By.XPATH, customer_contragent_add_xpath))).click()
-#
+# Выбираем Юр. Лицо
 search_element = wait.until(
 	EC.element_to_be_clickable((By.XPATH, flexbby_contragent_add_xpath))).click()
-action.double_click().perform()  # Двойной клик
+action.send_keys(Keys.RETURN).perform()  # Enter
+time.sleep(3)
+# Удаляем контрагента
+search_element = wait.until(
+	EC.element_to_be_clickable((By.XPATH, delete_contragent_button_xpath))).click()
+# Подтверждаем
+# Нажимаем Enter
+search_element = wait.until(
+	EC.element_to_be_clickable((By.XPATH, ok_delete_contract_xpath))).click()
 #
-#  Нажимаем кнопку Удалить договор
 time.sleep(15)
+# Делаем скриншот
+scr_name = today + "_" + "docID" + doc_id + "_" + "complex" + "_" + "test" + ".png"
+driver.save_screenshot('C:\Users\Operator\Desktop\Work\Tests\Screen\\%s' % scr_name)
+# Удаить договор
 search_element = wait.until(
 	EC.element_to_be_clickable((By.XPATH, delete_contract_xpath))).click()
 # Нажимаем Enter
