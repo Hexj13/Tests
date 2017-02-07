83b8f4481fa7bfc5e95c19f61513393f752ee2ab
Vladimir Lapchinsky <vladimir.lapchinsky@gmail.com>
2017-02-07 20:00:05 +0300

    comment

--- entity_testing/entity_test.py
+++ entity_testing/entity_test.py
@@ -8,11 +8,11 @@ from selenium.webdriver.common.by import By
 from elements_config import *
 from content_config import *
 
-#
+# Устанавливаем драйвер
 driver = webdriver.Chrome()
-#
+# Максимайз
 driver.maximize_window()
-#
+# Устанавливаем экшен
 action = action_chains.ActionChains(driver)
 # Установим тайм для драйвера
 wait = WebDriverWait(driver, 10)
@@ -30,24 +30,23 @@ search_element = driver.find_element_by_id(submit_button_id)
 search_element.click()
 # Проваливаемся на сайт
 time.sleep(two_sec)
-#
+# Находим в меню Юр. лица
 search_element = wait.until(
 	EC.element_to_be_clickable((By.XPATH, entity_menu_button_xpath))).click()
-#
+# Добавить
 search_element = wait.until(
 	EC.element_to_be_clickable((By.XPATH, add_button_xpath))).click()
-#
+# Вводим ИНН
 search_element = wait.until(
 	EC.element_to_be_clickable((By.XPATH, inn_input_xpath)))
 search_element.send_keys(inn)
-#
+# Нажимаем кнопку
 search_element = wait.until(
 	EC.element_to_be_clickable((By.ID, get_data_id))).click()
-#
+# Выбираем компанию
 search_element = wait.until(
 	EC.element_to_be_clickable((By.XPATH, company_window_xpath.format(inn=inn)))).click()
 #
-#TODO: COMMENTS!!!!
 
 """Delete&Close"""
 time.sleep(ten_sec)
