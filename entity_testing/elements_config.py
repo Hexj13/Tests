c6d8aa3951d12abbb1a6a7b49833d194a9ac446f
Vladimir Lapchinsky <vladimir.lapchinsky@gmail.com>
2017-02-09 19:42:28 +0300

    POW!
    09.02

--- entity_testing/elements_config.py
+++ entity_testing/elements_config.py
@@ -5,27 +5,72 @@
 #
 two_sec = 2
 #
+three_sec = 3
+#
+four_sec = 4
+#
 five_sec = 5
 #
 ten_sec = 10
 
 """"XPATH"""
+
+"""КНОПКИ"""
+
+#
+overall_button_xpath = "//div[contains(text(),'Общее')and not(ancestor::div[contains(@style," \
+                       "'display:none')])and not(ancestor::div[contains(@style,'display: none')])]"
+#
+contracts_button_xpath = "//div[@class = 'qx-flexbby-tabview-button-underlined']//div[contains(text(),'Договоры')and " \
+                         "not(ancestor::div[contains(@style," \
+                         "'display:none')])and not(ancestor::div[contains(@style,'display: none')])]"
+#
+accounts_button_xpath = "//div[@class = 'qx-flexbby-tabview-button-underlined']//div[contains(text(),'Счета')and " \
+                        "not(ancestor::div[contains(@style," \
+                        "'display:none')])and not(ancestor::div[contains(@style,'display: none')])]"
+#
+orders_button_xpath = "//div[@class = 'qx-flexbby-tabview-button-underlined']//div[contains(text(),'Заказы')and " \
+                      "not(ancestor::div[contains(@style," \
+                      "'display:none')])and not(ancestor::div[contains(@style,'display: none')])]"
+#
+employees_button_xpath = "//div[contains(text(),'Сотрудники')and not(ancestor::div[contains(@style," \
+                         "'display:none')])and not(ancestor::div[contains(@style,'display: none')])]"
 #
 entity_menu_button_xpath = "//div[text() = 'Юр. лица']"
 #
-add_button_xpath = ".//div[.='Добавить' and not(ancestor::div[contains(@style,'display:none')])and not(" \
+add_button_xpath = "//div[text() ='Добавить' and not(ancestor::div[contains(@style,'display:none')])and not(" \
                    "ancestor::div[contains(@style,'display: none')])]"
 #
 delete_button_xpath = "//div[@id = 'deleteb' and not(ancestor::div[contains(@style," \
                       "'display:none')])and not(ancestor::div[contains(@style,'display: none')])]"
 #
-ok_delete_button_xpath = "//div[@class = 'qx-window']//div[contains(text(), 'OK')]"
+ok_button_window_xpath = "//div[@class = 'qx-window']//div[contains(text(), 'OK')]"
+#
+ok_button_xpath = "//div[@id = 'okb' and not(ancestor::div[contains(@style," \
+                  "'display:none')])and not(ancestor::div[contains(@style,'display: none')])]"
+#
+recvisits_button_xpath = "//div[contains(text(),'Реквизиты')and not(ancestor::div[contains(@style," \
+                         "'display:none')])and not(ancestor::div[contains(@style,'display: none')])]"
+#
+activity_type_button_xpath = "//div[@id = 'CompanyActivityType_objectID'and not(ancestor::div[contains(@style," \
+                             "'display:none')])and not(ancestor::div[contains(@style,'display: none')])]//div[@class = " \
+                             "'qx-button-common-border']"
+#
+employee_delete_button_xpath = "//div[@class = 'qx-button-common-border-middle' and not(ancestor::div[contains(@style," \
+                               "'display:none')])and not(ancestor::div[contains(@style,'display: none')])]//div[" \
+                               "contains(text(), 'Удалить')] "
+"""ИНПУТЫ"""
+#
+doc_date_input_xpath = "//div[@id='docDate']//input"
+#
+contracts_type_input_xpath = ".//div[@id='documentTypeID' and not(ancestor::div[contains(@style,'display:none')])and " \
+                             "not(ancestor::div[contains(@style,'display: none')])]//input "
+#
+account_type_input_xpath = ".//div[@id='planTypeID' and not(ancestor::div[contains(@style,'display:none')])and " \
+                           "not(ancestor::div[contains(@style,'display: none')])]//input"
 #
 inn_input_xpath = "//input[@id = 'inn' and not(ancestor::div[contains(@style," \
                   "'display:none')])and not(ancestor::div[contains(@style,'display: none')])]"
-#
-company_window_xpath = "//div[@class = 'qx-window' and not(ancestor::div[contains(@style," \
-                       "'display:none')])and not(ancestor::div[contains(@style,'display: none')])][2]"
 #
 site_input_xpath = "//input[@id = 'url' and not(ancestor::div[contains(@style," \
                    "'display:none')])and not(ancestor::div[contains(@style,'display: none')])]"
@@ -42,9 +87,6 @@ deactivateDate_input_xpath = "//div[@id = 'deactivateDate' and not(ancestor::div
 comment_input_xpath = "//input[@id = 'comment' and not(ancestor::div[contains(@style," \
                       "'display:none')])and not(ancestor::div[contains(@style,'display: none')])]"
 #
-recvisits_button_xpath = "//div[contains(text(),'Реквизиты')and not(ancestor::div[contains(@style," \
-                         "'display:none')])and not(ancestor::div[contains(@style,'display: none')])]"
-#
 okpo_input_xpath = "//input[@id = 'okpo' and not(ancestor::div[contains(@style," \
                    "'display:none')])and not(ancestor::div[contains(@style,'display: none')])]"
 #
@@ -57,24 +99,66 @@ foreign_name_input_xpath = "//input[@id = 'foreignLegalName' and not(ancestor::d
 street_name_input_xpath = "//input[@id = 'streetAddress' and not(ancestor::div[contains(@style," \
                           "'display:none')])and not(ancestor::div[contains(@style,'display: none')])]"
 #
-proposed_address_div_xpath = "//div[@id = 'proposed_addresses' and not(ancestor::div[contains(@style," \
-                             "'display:none')])and not(ancestor::div[contains(@style,'display: none')])]//a"
-#
-activity_type_button_xpath = "//div[@id = 'CompanyActivityType_objectID'and not(ancestor::div[contains(@style," \
-                             "'display:none')])and not(ancestor::div[contains(@style,'display: none')])]//div[@class = " \
-                             "'qx-button-common-border']"
-#
 code_activity_type_input_xpath = "//input[@id = 'code' and not(ancestor::div[contains(@style," \
                                  "'display:none')])and not(ancestor::div[contains(@style,'display: none')])]"
 #
 name_activity_type_input_xpath = "//input[@id = 'name' and not(ancestor::div[contains(@style," \
                                  "'display:none')])and not(ancestor::div[contains(@style,'display: none')])]"
+
+"""ОСТАЛЬНОЕ"""
 #
-overall_button_xpath = "//div[contains(text(),'Общее')and not(ancestor::div[contains(@style," \
+employee_table_xpath = "//parent::div[@class = 'qooxdoo-table-cell' and not(ancestor::div[contains(@style," \
                        "'display:none')])and not(ancestor::div[contains(@style,'display: none')])]"
 #
-employees_button_xpath = "//div[contains(text(),'Сотрудники')and not(ancestor::div[contains(@style," \
-                         "'display:none')])and not(ancestor::div[contains(@style,'display: none')])]"
+company_window_xpath = "//div[@class = 'qx-window' and not(ancestor::div[contains(@style," \
+                       "'display:none')])and not(ancestor::div[contains(@style,'display: none')])][2]"
+#
+proposed_address_div_xpath = "//div[@id = 'proposed_addresses' and not(ancestor::div[contains(@style," \
+                             "'display:none')])and not(ancestor::div[contains(@style,'display: none')])]//a"
+#
+popup_menu_select_xpath = "//div[@class='qx-popup' and not(ancestor::div[contains(@style," \
+                          "'display:none')])and not(ancestor::div[contains(@style,'display: none')])]//div[text()='%s']"
+
+"""БАНКОВСКИЕ РЕКВИЗИТЫ XPATH"""
+
+add_bank_button_xpath = "//div[@class = 'qx-button-leading-border-left' and not(ancestor::div[contains(@style," \
+                        "'display:none')])and not(ancestor::div[contains(@style,'display: none')])]//div[text() = " \
+                        "'Добавить'] "
+#
+text_comment_bank_xpath = "//textarea[@id = 'comment' and not(ancestor::div[contains(@style," \
+                          "'display:none')])and not(ancestor::div[contains(@style,'display: none')])]"
+#
+inn_bank_xpath = "//div[@class = 'qx-window' and not(ancestor::div[contains(@style," \
+                 "'display:none')])and not(ancestor::div[contains(@style,'display: none')])]//input[@id = 'inn']"
+#
+bik_bank_xpath = "//div[@class = 'qx-window' and not(ancestor::div[contains(@style," \
+                 "'display:none')])and not(ancestor::div[contains(@style,'display: none')])]//input[@id = 'bik']"
+#
+kpp_bank_xpath = "//div[@class = 'qx-window' and not(ancestor::div[contains(@style," \
+                 "'display:none')])and not(ancestor::div[contains(@style,'display: none')])]//input[@id = 'kpp']"
+#
+inn_bank_xpath = "//div[@class = 'qx-window' and not(ancestor::div[contains(@style," \
+                 "'display:none')])and not(ancestor::div[contains(@style,'display: none')])]//input[@id = 'inn']"
+#
+rs_bank_xpath = "//div[@class = 'qx-window' and not(ancestor::div[contains(@style," \
+                "'display:none')])and not(ancestor::div[contains(@style,'display: none')])]//input[@id = " \
+                "'accountNumber']"
+#
+ls_bank_xpath = "//div[@class = 'qx-window' and not(ancestor::div[contains(@style," \
+                "'display:none')])and not(ancestor::div[contains(@style,'display: none')])]//input[@id = " \
+                "'personalAccount']"
+#
+name_foreign_bank_xpath = "//div[@class = 'qx-window' and not(ancestor::div[contains(@style," \
+                          "'display:none')])and not(ancestor::div[contains(@style,'display: none')])]//input[@id = " \
+                          "'nameForeign']"
+#
+main_bank_xpath = "//div[@class = 'qx-window' and not(ancestor::div[contains(@style," \
+                  "'display:none')])and not(ancestor::div[contains(@style,'display: none')])]//div[@id = " \
+                  "'main']"
+#
+deactivateDate_bank_xpath = "//div[@class = 'qx-window' and not(ancestor::div[contains(@style," \
+                            "'display:none')])and not(ancestor::div[contains(@style,'display: none')])]//div[@id = " \
+                            "'deactivateDate']//input"
 """"ID"""
 #
 login_id = 'login'
@@ -84,3 +168,6 @@ password_id = 'password'
 submit_button_id = 'enter'
 #
 get_data_id = 'getCompanyData'
+#
+shadow_id = 'shadow'
+#
--- entity_testing/entity_test.py
+++ entity_testing/entity_test.py
@@ -18,7 +18,7 @@ driver.maximize_window()
 # Устанавливаем экшен
 action = action_chains.ActionChains(driver)
 # Установим тайм для драйвера
-wait = WebDriverWait(driver, 10)
+wait = WebDriverWait(driver, 30)
 # Заходим на сайт
 driver.get(site_url)
 # Вводим логин
@@ -78,6 +78,9 @@ search_element.send_keys(tomorrow_str)
 search_element = driver.find_element_by_xpath(comment_input_xpath).send_keys(comment_text)
 
 """"РЕКВИЗИТЫ"""
+# Проверяем на отсутвие shadow
+search_element = wait.until(
+	EC.invisibility_of_element_located((By.ID, shadow_id)))
 # Нажимаем Реквизиты
 search_element = wait.until(
 	EC.element_to_be_clickable((By.XPATH, recvisits_button_xpath))).click()
@@ -115,24 +118,139 @@ search_element = wait.until(
 	EC.element_to_be_clickable((By.XPATH, comment_input_xpath)))
 search_element.send_keys(comment_text)
 
-"""Удаление ссылок"""
+"""БАНКОВСКИЕ РЕКВИЗИТЫ"""
+# Нажимаем Добавить банковский реквизит
+search_element = wait.until(
+	EC.element_to_be_clickable((By.XPATH, add_bank_button_xpath))).click()
+# Вводим БИК
+search_element = wait.until(
+	EC.element_to_be_clickable((By.XPATH, bik_bank_xpath)))
+search_element.send_keys(bik_number_text)
+# Вводим Имя банка
+search_element = driver.find_element_by_xpath(name_foreign_bank_xpath).send_keys(bank_name_text)
+# Вводим ИНН
+search_element = driver.find_element_by_xpath(inn_bank_xpath).send_keys(inn_bank_text)
+# Вводим КПП
+search_element = driver.find_element_by_xpath(kpp_bank_xpath).send_keys(kpp_bank_text)
+# Вводим Р/с Банка
+search_element = driver.find_element_by_xpath(rs_bank_xpath).send_keys(rs_bank_text)
+# Вводим Л/с Банка
+search_element = driver.find_element_by_xpath(ls_bank_xpath).send_keys(ls_bank_text)
+# Проставляем Основной счёт
+search_element = driver.find_element_by_xpath(main_bank_xpath).click()
+# Вводим дату диактивации
+search_element = driver.find_element_by_xpath(deactivateDate_bank_xpath).send_keys(tomorrow_str)
+# Вводим коммент
+search_element = driver.find_element_by_xpath(text_comment_bank_xpath).send_keys(comment_text)
+# Нажимаем ОК
+search_element = driver.find_element_by_xpath(ok_button_window_xpath).click()
+
+"""ДОГОРОВЫ"""
+# Проверяем на отсутвие shadow
+search_element = wait.until(
+	EC.invisibility_of_element_located((By.ID, shadow_id)))
+# Нажимаем Договоры в меню
+search_element = wait.until(
+	EC.element_to_be_clickable((By.XPATH, contracts_button_xpath))).click()
+# Нажимаем Добавить
+search_element = wait.until(
+	EC.element_to_be_clickable((By.XPATH, add_button_xpath))).click()
+# Находим поле Типа документа
+search_element = wait.until(
+	EC.element_to_be_clickable((By.XPATH, contracts_type_input_xpath)))
+# Вводим в поле текст
+contract_type_name = unicode(contracts_type_name.decode("utf-8"))
+search_element.send_keys(contract_type_name)
+# Находим и нажимаем в списке нужный тип документа
+search_element = wait.until(
+	EC.element_to_be_clickable((By.XPATH, popup_menu_select_xpath % contract_type_name))).click()
+# Проставляем дату документа
+search_element = wait.until(
+	EC.element_to_be_clickable((By.XPATH, doc_date_input_xpath)))
+search_element.send_keys(today_str)
+search_element.send_keys(Keys.RETURN)
+# Спим
+time.sleep(five_sec)
+# Закрываем договор
+search_element = wait.until(
+	EC.element_to_be_clickable((By.XPATH, ok_button_xpath))).click()
+time.sleep(four_sec)
+
+"""ЗАКАЗЫ"""
+# Проверяем на отсутвие shadow
+search_element = wait.until(
+	EC.invisibility_of_element_located((By.ID, shadow_id)))
+# Нажимаем Заказы в меню
+search_element = wait.until(
+	EC.element_to_be_clickable((By.XPATH, orders_button_xpath))).click()
+# Нажимаем Добавить
+search_element = wait.until(
+	EC.element_to_be_clickable((By.XPATH, add_button_xpath))).click()
+# Проставляем дату документа
+search_element = wait.until(
+	EC.element_to_be_clickable((By.XPATH, doc_date_input_xpath)))
+search_element.send_keys(today_str)
+search_element.send_keys(Keys.RETURN)
+# Спим
+time.sleep(five_sec)
+# Закрываем заказ
+search_element = wait.until(
+	EC.element_to_be_clickable((By.XPATH, ok_button_xpath))).click()
+time.sleep(four_sec)
+
+"""СЧЕТА"""
+# Проверяем на отсутвие shadow
+search_element = wait.until(
+	EC.invisibility_of_element_located((By.ID, shadow_id)))
+# Нажимаем Счета в меню
+search_element = wait.until(
+	EC.element_to_be_clickable((By.XPATH, accounts_button_xpath))).click()
+# Нажимаем Добавить
+search_element = wait.until(
+	EC.element_to_be_clickable((By.XPATH, add_button_xpath))).click()
+# Находим поле Типа счёта
+search_element = wait.until(
+	EC.element_to_be_clickable((By.XPATH, account_type_input_xpath)))
+# Вводим в поле текст
+account_type_name = unicode(account_type_name.decode("utf-8"))
+search_element.send_keys(account_type_name)
+# Находим и нажимаем в списке нужный тип счёта
+search_element = wait.until(
+	EC.element_to_be_clickable((By.XPATH, popup_menu_select_xpath % account_type_name))).click()
+# TODO: ВЫБОР ДОКУМЕНТА
+
+"""УДАЛЕНИЕ ССЫЛОК"""
+# Проверяем на отсутвие shadow
+search_element = wait.until(
+	EC.invisibility_of_element_located((By.ID, shadow_id)))
 # Прохоим в Общее
-search_element = driver.find_element_by_xpath(overall_button_xpath).click()
+search_element = wait.until(
+	EC.element_to_be_clickable((By.XPATH, overall_button_xpath))).click()
 # Проходим в Сотрудники
 search_element = wait.until(
 	EC.element_to_be_clickable((By.XPATH, employees_button_xpath))).click()
+# Выбираем сотрудника ссылающегося на док
+search_element = wait.until(
+	EC.element_to_be_clickable((By.XPATH, employee_table_xpath))).click()
+# Нажимаем Удалить
+search_element = driver.find_element_by_xpath(employee_delete_button_xpath).click()
+# Нажимаем ОК
+search_element = wait.until(
+	EC.element_to_be_clickable((By.XPATH, ok_button_window_xpath))).click()
+# Спим
+time.sleep(two_sec)
 
 """Delete&Screen&Close"""
 # Делаем скриншот
-scr_name = today_str + "_" + "docID" + doc_id + "_" + "entity" + "_" + "test" + ".jpeg"
+scr_name = today_str + "_" + "docID" + doc_id + "_" + "entity" + "_" + "test" + ".png"
 driver.save_screenshot('C:\Users\Operator\Desktop\Testing\Tests\screens\\%s' % scr_name)
-time.sleep(ten_sec)
+time.sleep(two_sec)
 # Удаить договор
 search_element = wait.until(
 	EC.element_to_be_clickable((By.XPATH, delete_button_xpath))).click()
 # Нажимаем Enter
 search_element = wait.until(
-	EC.element_to_be_clickable((By.XPATH, ok_delete_button_xpath))).click()
+	EC.element_to_be_clickable((By.XPATH, ok_button_window_xpath))).click()
 # Закрыть Хром
 time.sleep(two_sec)
 driver.close()
